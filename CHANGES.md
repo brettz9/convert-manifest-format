962dbbb0695d8fbc5992db7f8a2dab046e9904bd
Brett Zamir <brettz9@yahoo.com>
2023-04-03 14:41:40 -0700

    fix: path bug

---
+++ CHANGES.md
@@ -0,0 +1,13 @@
+# CHANGES for `convert-manifest-format`
+
+## 0.2.1
+
+- fix: path bug
+
+## 0.2.0
+
+- feat: comments out or uncomments polyfill import statement
+
+## 0.1.0
+
+- Initial version
--- bin/index.js
+++ bin/index.js
@@ -10,17 +10,16 @@ const manifest = JSON.parse(file);
 
 if (process.argv.includes('--chrome')) {
   manifest.background.service_worker = manifest.background.scripts[0];
-  delete manifest.background.scripts;
   const workerFile = await readFile(manifest.background.scripts[0]);
   workerFile = workerFile.replace(/^\/\/ (import .*browser-polyfill.min.js['"];?)/um, '$1');
   await writeFile(manifest.background.scripts[0], workerFile);
-
+  delete manifest.background.scripts;
 } else if (process.argv.includes('--firefox')) {
   manifest.background.scripts = [manifest.background.service_worker];
-  delete manifest.background.service_worker;
   const workerFile = await readFile(manifest.background.service_worker);
   workerFile = workerFile.replace(/^(import .*browser-polyfill.min.js['"];?)/um, '// $1');
   await writeFile(manifest.background.service_worker, workerFile);
+  delete manifest.background.service_worker;
 }
 
 const manifestString = JSON.stringify(manifest, null, 2) + '\n';
